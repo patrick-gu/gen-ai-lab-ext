d0e63ba15bc34d15ae19e3a516bc70ef730c52f1
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-08-27 06:46:14 +0700

    restored more of main

--- main.js
+++ main.js
@@ -4,76 +4,24 @@ import {
   ConverseCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 
-// using these models since anthropic requires us to fork over some info and the results are basically the same
-const llama3Id = "meta.llama3-1-8b-instruct-v1:0";
-const mistralId = "mistral.mistral-7b-instruct-v0:2";
+// const modelId = 'anthropic.claude-3-haiku-20240307-v1:0';
+const modelId = "anthropic.claude-v2";
+const prompt =
+  "Give me an affirmation to boost my motivation today, referencing plants, animals, or flowers by adding emoji. Don't show the prompt, only the quote. Do not add anything like Here is an affirmation... just return the affirmation alone";
+const conversation = [
+  {
+    role: "user",
+    content: [{ text: prompt }],
+  },
+];
 
-let selectedChips = [];
-
-// generated prompt when function is called with current chips
-function getLLMInput() {
-  const prompt =
-    "Give me an affirmation to boost my motivation today" +
-      selectedChips.length ===
-    0
-      ? "."
-      : ", referencing " +
-        selectedChips.join(" and ") +
-        " by adding emoji." +
-        " Don't show the prompt, only the quote. Do not add anything like Here is an affirmation... just return the affirmation alone";
-  return [
-    {
-      role: "user",
-      content: [{ text: prompt }],
-    },
-  ];
-}
-
-// generated prompt when function is called with current chips
-function getImageGenInput() {
-  const imagePrompt =
-    "Give me motivatonal image" + selectedChips.length === 0
-      ? ""
-      : "featuring " + selectedChips.join(" and ");
-  return {
-    contentType: "application/json",
-    accept: "*/*",
-    modelId: "stability.stable-diffusion-xl-v1",
-    body: JSON.stringify({
-      text_prompts: [
-        {
-          text: imagePrompt,
-        },
-      ],
-      cfg_scale: 5,
-      seed: Math.floor(Math.random() * 1025),
-      steps: 50,
-    }),
-  };
-}
-
-// toggles styling and adds/removes elements from selectedChips
-function toggleChips(id) {
-  const chip = document.querySelector("#" + id);
-  if (selectedChips.indexOf(id) == -1) {
-    chip.classList.remove("deselected");
-    selectedChips.push(id);
-    console.log(selectedChips);
-    return;
-  }
-
-  chip.classList.add("deselected");
-  selectedChips = selectedChips.filter((item) => item != id);
-  console.log(selectedChips);
-}
-
-async function fetchNewAffirmation(modelId) {
+async function fetchNewAffirmation() {
   disableButton(true);
   showLoadingAnimation();
 
   try {
     const response = await client.send(
-      new ConverseCommand({ modelId, messages: getLLMInput() }),
+      new ConverseCommand({ modelId, messages: conversation }),
     );
     const affirmation = response.output.message.content[0].text;
     // set the affirmation in HTML
@@ -81,58 +29,9 @@ async function fetchNewAffirmation(modelId) {
   } catch (err) {
     console.error(err);
     document.querySelector("#affirmation").innerHTML = err;
-    disableButton(false);
-    throw err; // propogate error up, I know this is kinda stupid but I'm also way too lazy to do this properly
-  }
-  disableButton(false);
-}
-
-const textDecoder = new TextDecoder("utf-8");
-
-// generate image using SDXL
-async function generateImage() {
-  disableButton(true);
-  showImageLoadingAnimation();
-
-  try {
-    const response = await client.send(
-      new InvokeModelCommand(getImageGenInput()),
-    );
-    const jsonString = textDecoder.decode(response.body.buffer);
-    const parsedData = JSON.parse(jsonString);
-    document.querySelector("#imageContainer").innerHTML =
-      `<img src=\"data:image/png;base64, ${parsedData.artifacts[0].base64}\">`; // this part will be different for Titan image gen
-  } catch (err) {
-    document.querySelector("#affirmation").innerHTML = err; // show error in affirmation
-    document.querySelector("#imageContainer").innerHTML = ""; // clear image
-    disableButton(false);
   }
 
   disableButton(false);
-}
-
-// generate affirmation with Llama w/ mistral fallback
-async function generateLlama() {
-  try {
-    await fetchNewAffirmation(llama3Id);
-  } catch (err) {
-    document.querySelector("#affirmation").innerHTML +=
-      "\nRetrying with other model";
-    await new Promise((r) => setTimeout(r, 1000)); // so user has time to read error before falling back to other model
-    await fetchNewAffirmation(mistralId); // fallback model
-  }
-}
-
-// generate affirmation with mistral w/ Llama fallback
-async function generateMistral() {
-  try {
-    await fetchNewAffirmation(mistralId);
-  } catch (err) {
-    document.querySelector("#affirmation").innerHTML +=
-      "\nRetrying with other model";
-    await new Promise((r) => setTimeout(r, 1000)); // so user has time to read error before falling back to other model
-    await fetchNewAffirmation(llama3Id); // fallback model
-  }
 }
 
 // Shows a loading animation while fetching a new affirmation
@@ -141,20 +40,10 @@ function showLoadingAnimation() {
     '<div class="loading-spinner"></div>';
 }
 
-// Shows a loading animation while generateing a new image
-function showImageLoadingAnimation() {
-  document.querySelector("#imageContainer").innerHTML =
-    '<div class="loading-spinner"></div>';
-}
-
 // Disables the button while fetching a new affirmation so we don't request several at once by clicking repeatedly
 function disableButton(isDisabled) {
-  const llamaButton = document.querySelector("#getNewAffirmationLlama");
-  const mistralButton = document.querySelector("#getNewAffirmationMistral");
-  const imageButton = document.querySelector("#generateImage");
-  llamaButton.disabled = isDisabled;
-  mistralButton.disabled = isDisabled;
-  imageButton.disabled = isDisabled;
+  const affirmationButton = document.querySelector("#getNewAffirmation");
+  affirmationButton.disabled = isDisabled;
 }
 
 init();
@@ -165,48 +54,34 @@ async function init() {
     // get the user's credentials from environment variables
     const creds = await fetchCredentials();
     // instantiate the BedrockRuntimeClient
-    client = createBedrockClient(creds);
+    client = await createBedrockClient(creds);
     // Once everything is setup, let's get the first affirmation
-    await generateLlama();
+    await fetchNewAffirmation();
   } catch (err) {
     console.error(err);
     document.querySelector("#affirmation").innerHTML = err;
   }
 
-  // binding buttons to functions
-  const llamaButton = document.querySelector("#getNewAffirmationLlama");
-  llamaButton.addEventListener("click", generateLlama);
-  const mistralButton = document.querySelector("#getNewAffirmationMistral");
-  mistralButton.addEventListener("click", generateMistral);
-  const imageButton = document.querySelector("#generateImage");
-  imageButton.addEventListener("click", generateImage);
-
-  // binding chips to functions
-  const flowersChip = document.querySelector("#flowers");
-  flowersChip.addEventListener("click", () => toggleChips("flowers"));
-  const plantsChip = document.querySelector("#plants");
-  plantsChip.addEventListener("click", () => toggleChips("plants"));
-  const animalsChip = document.querySelector("#animals");
-  animalsChip.addEventListener("click", () => toggleChips("animals"));
+  const affirmationButton = document.querySelector("#getNewAffirmation");
+  affirmationButton.addEventListener("click", fetchNewAffirmation);
 }
 
 let client = null;
-// Initialize new Bedrock client
-function createBedrockClient(creds) {
-  client = new BedrockRuntimeClient({
+async function createBedrockClient(creds) {
+  client = await new BedrockRuntimeClient({
     credentials: creds.credentials,
     region: creds.region,
   });
   return client;
 }
 
-// get creds from .env file
 async function fetchCredentials() {
   return {
     region: "us-west-2", // Hardcoded as this region is a requirement for the hosted Workshops and must not be changed.
     credentials: {
       accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
       secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+      sessionToken: import.meta.env.VITE_AWS_SESSION_TOKEN,
     },
   };
 }
